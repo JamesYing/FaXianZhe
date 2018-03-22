d2140400e08be14c0dbbcf892d88e801a49e156e
jamesying <james@taogame.com>
2018-03-22 17:38:08 +0800

    add unit test for cacheProvider and cacheProviderFactory

---
+++ test/JCSoft.FXZ.test.Server/Cache/DefaultCacheProviderFactoryTests.cs
@@ -0,0 +1,73 @@
+﻿using Shouldly;
+using Xunit;
+using TestStack.BDDfy;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using FXZServer.Cache;
+using Microsoft.Extensions.Logging;
+using FXZServer.Configurations;
+using FXZServer.Cache.Providers;
+using System;
+
+namespace JCSoft.FXZ.test.Server.Cache
+{
+    public class DefaultCacheProviderFactoryTests
+    {
+        private Mock<IMemoryCache> _mockMemoryCache;
+        private Mock<ILoggerFactory> _mockLoggerFactory;
+        private ICacheProviderFactory _cacheProviderFactory;
+        private ICacheProvider _cacheProvider;
+        private Exception _ex;
+
+        public DefaultCacheProviderFactoryTests()
+        {
+            _mockLoggerFactory = new Mock<ILoggerFactory>();
+            _mockMemoryCache = new Mock<IMemoryCache>();
+        }
+
+        [Fact]
+        public void Should_Create_WebCache_Success()
+        {
+            this.Given(s => WhenCreateCacheProvider())
+                .When(s => WhenCacheModeIs(CacheMode.WebCache))
+                .Then(s => ThenCacheProvoiderTypeIs<MemoryCacheProvider>())
+                .BDDfy();
+        }
+
+        [Fact]
+        public void Should_Create_CacheProvider_Failed()
+        {
+            this.Given(s => WhenCreateCacheProvider())
+                .When(s => WhenCacheModeIs(CacheMode.Redis))
+                .Then(s => ThenThrowAnException())
+                .BDDfy();
+        }
+
+        private void ThenThrowAnException()
+        {
+            _ex.ShouldNotBeNull();
+        }
+
+        private void ThenCacheProvoiderTypeIs<T>()
+        {
+            _cacheProvider.ShouldBeOfType<T>();
+        }
+
+        private void WhenCacheModeIs(CacheMode cacheMode)
+        {
+            try
+            {
+                _cacheProvider = _cacheProviderFactory.CreateProvider(cacheMode);
+            }
+            catch(Exception ex)
+            {
+                _ex = ex;
+            }
+        }
+
+        private void WhenCreateCacheProvider()
+        {
+            _cacheProviderFactory = new DefaultCacheProviderFactory(_mockMemoryCache.Object, _mockLoggerFactory.Object);
+        }
+    }
+}
---
+++ test/JCSoft.FXZ.test.Server/Cache/Providers/MemoryCacheProviderTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shouldly;
+using Xunit;
+using TestStack.BDDfy;
+using Moq;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using FXZServer.Cache.Providers;
+
+namespace JCSoft.FXZ.test.Server.Cache.Providers
+{
+    public class MemoryCacheProviderTests
+    {
+        private Mock<IMemoryCache> _memoryCache;
+        private Mock<ILoggerFactory> _loggerFactory;
+        private Dictionary<string, object> _dict;
+        private ICacheProvider _cacheProvider;
+        private Mock<ICacheEntry> _cacheEntry;
+
+        public MemoryCacheProviderTests()
+        {
+            _memoryCache = new Mock<IMemoryCache>();
+            _loggerFactory = new Mock<ILoggerFactory>();
+            _dict = new Dictionary<string, object>();
+            _cacheProvider = new MemoryCacheProvider(_memoryCache.Object, _loggerFactory.Object);
+            _cacheEntry = new Mock<ICacheEntry>();
+        }
+
+        [Fact]
+        public void Should_SaveAndGet_To_Cache()
+        {
+            var key = "key";
+            var value = "value";
+            this.Given(s => s.GivenCacheProviderInit(key, value))
+                .When(s => s.WhenCacheProviderSaveKeyValue(key, value))
+                .Then(s => s.ThenGetValueFromCacheIs(key, value))
+                .BDDfy();
+        }
+
+        private void ThenGetValueFromCacheIs(string key, object obj)
+        {
+            var value = _cacheProvider.Get<string>(key);
+            value.ShouldBe(obj.ToString());
+        }
+
+        private void WhenCacheProviderSaveKeyValue(string key, object obj)
+        {
+            _cacheProvider.Save(key, obj, DateTimeOffset.MaxValue);
+        }
+
+        private void GivenCacheProviderInit(string key, object obj)
+        {
+            _memoryCache.Setup(s => s.CreateEntry(key)).Returns(() =>
+            {
+                _dict.Add(key, obj);
+                return _cacheEntry.Object;
+            });
+
+            //object outobj = null;
+            _memoryCache.Setup(s => s.TryGetValue(key, out obj))
+                .Returns(() =>
+                {
+                    return _dict.TryGetValue(key, out obj);
+                });
+        }
+    }
+}
